d972a802db6ead71d77a8d0dd2f2b196db43532d
GiftSibiya <sibiya.bobo@gmail.com>
2024-01-15 00:11:45 +0200

    Header and styles

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,15 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import { Feather } from "react-native-vector-icons";
 
 const Header = () => {
   return (
     <View style={styles.container}>
-      <Text>Header</Text>
+      <View style={styles.HeaderContainer}>
+        <Text style={styles.headerText}> WhatsApp</Text>
+        <View style={styles.iconContainer}></View>
+        <Feather style={styles.icons} name="camera" size={20} color="white" />
+      </View>
     </View>
   );
 };
@@ -14,8 +19,15 @@ export default Header;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#0e806a",
+    paddingTop: 80,
   },
+  HeaderContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginHorizontal: 16,
+  },
+  iconContainer: {},
+  icons: {},
 });
